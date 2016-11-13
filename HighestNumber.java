57c27195f5768ad3fbe8e44ea6ee32ac9e34d39c
akhilsharma6 <akhilsharma69@yahoo.co.in>
2016-11-13 23:12:54 +0530

    Assignment 2.4 complete

--- HighestNumber.java
+++ HighestNumber.java
@@ -1,23 +1,28 @@
+/* Program to display highest of 3 numbers */
+
 public class HighestNumber {
 
 	public static void main (String[] args)
 	{
+		//Receive three inputs from console.
 		System.out.print("Enter 1st number: ");
 		int a = Integer.parseInt(System.console().readLine());
 		System.out.print("Enter 2nd number: ");
 		int b = Integer.parseInt(System.console().readLine());
 		System.out.print("Enter 3rd number: ");
 		int c = Integer.parseInt(System.console().readLine());
+		//Declare object
 		MaxResult mn = new MaxResult();
-		System.out.println(mn.highCalc(a,b,c));
+		//Display the highest number after calling the highCalc method.
+		System.out.println(mn.highCalc(a,b,c));	// Pass input by reference.
 	}			
 }
-
+// Class to define MaxResult logic
 class MaxResult {
-	public String highCalc(int x, int y, int z) {
-		if ((x >= y) && (x >= z))
+	public String highCalc(int x, int y, int z) { //Method receives three inputs
+		if ((x >= y) && (x >= z))	// if logic, checks if x is greater among the other two.
 		{
-			return "The highest number is " + String.valueOf(x);
+			return "The highest number is " + String.valueOf(x);	// Because return type is String, so using String.valueOf method to convert Int to String.
 		}
 		if ((y >= x) && (y >= z))
 		{
